f19b080c9ce895bf4ef70a8566467c78412ef89a
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-21 10:08:46 +0500

    why commit

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Why() {
     return (
         <div className="Why">
-            <div className='Block'>
+            <div className='Block whyBlock'>
                 <center><p className="subHead">/ why cipher</p></center>
                 <center><p className="heading blockHead">We provide tailored cybersecurity solutions driven by innovation and customer satisfaction</p></center>
                 <div className="links servicesLinks" style={{ justifyContent: "center" }}>
@@ -11,6 +11,72 @@ function Why() {
                     <a href="#">what others say</a>
                 </div>
             </div>
+            <div className="whySlideshow">
+                <div className='aboutSlides whySlides'>
+                    <div className='slidesMain'>
+                        <p>Protection</p>
+                        <p>Monitoring</p>
+                        <p>Expertise</p>
+                        <p>Customization</p>
+                        <p>Response</p>
+                        <p>Compliance</p>
+                        <p>Reliability</p>
+                        <p>Scalability</p>
+                        <p>Protection</p>
+                        <p>Monitoring</p>
+                        <p>Expertise</p>
+                        <p>Customization</p>
+                        <p>Response</p>
+                        <p>Compliance</p>
+                        <p>Reliability</p>
+                        <p>Scalability</p>
+                    </div>
+                </div>
+                <br />
+                <div className='aboutSlides whySlides'>
+                    <div className='slidesMain slideTwo'>
+                        <p>Support</p>
+                        <p>Efficiency</p>
+                        <p>Innovation</p>
+                        <p>Accessibility</p>
+                        <p>Integration</p>
+                        <p>Transparency</p>
+                        <p>Resilience</p>
+                        <p>Empowerment</p>
+                        <p>Support</p>
+                        <p>Efficiency</p>
+                        <p>Innovation</p>
+                        <p>Accessibility</p>
+                        <p>Integration</p>
+                        <p>Transparency</p>
+                        <p>Resilience</p>
+                        <p>Empowerment</p>
+                    </div>
+                </div>
+                <br />
+                <div className='aboutSlides whySlides'>
+                    <div className='slidesMain'>
+                        <p>Security</p>
+                        <p>Confidentiality</p>
+                        <p>Automation</p>
+                        <p>Performance</p>
+                        <p>Adaptability</p>
+                        <p>Simplicity</p>
+                        <p>Collaboration</p>
+                        <p>Insights</p>
+                        <p>Security</p>
+                        <p>Confidentiality</p>
+                        <p>Automation</p>
+                        <p>Performance</p>
+                        <p>Adaptability</p>
+                        <p>Simplicity</p>
+                        <p>Collaboration</p>
+                        <p>Insights</p>
+                    </div>
+                </div>
+            </div>
+
+            <center><span className='whySpan'>Our skilled team uses cutting-edge technology and tailored strategies to protect clients from evolving threats.</span></center>
         </div>
     )
 }
