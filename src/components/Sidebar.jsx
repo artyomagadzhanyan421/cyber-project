7d8f4c442f70e0721752a7673c2e29c14e0a58ec
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-22 11:44:05 +0500

    sidebar commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,8 +5,8 @@ import "../styles/Sidebar.css";
 
 function Sidebar({ click, effect }) {
     return (
-        <div className='Sidebar'>
-            <ul className={effect ? "sideLinks pop" : "sideLinks"}>
+        <div className={effect ? "Sidebar pop" : "Sidebar"}>
+            <ul>
                 <li><a href="#about" onClick={click}>About</a></li>
                 <li><a href="#services" onClick={click}>Solutions</a></li>
                 <li><a href="#feedbacks" onClick={click}>Testimonials</a></li>
